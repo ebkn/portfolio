af60c3add6891abe1fe4caf082f5c9fdf95392f2
ebkn <ktennis.mqekr12@gmail.com>
2019-04-18 23:17:51 +0900

    feat: move required files to build/ or src/

--- service-worker.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable */
-self.addEventListener('install', function() {
-  console.log('Service Worker install');
-});
-self.addEventListener('activate', function() {
-  console.log('Service Worker Activate');
-});
-self.addEventListener('fetch', function(event) {});
