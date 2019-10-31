74b2e0fbcbcba258021d51d00b81347417841469
ebkn <ktennis.mqekr12@gmail.com>
2019-11-01 01:12:01 +0900

    fix: typeerror

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import * as preact from 'preact';
+import { h } from 'preact';
 import 'normalize.css';
 import App from './app';
 
-/* @jsx preact.h */
+/* @jsx h */
 /* eslint-disable-next-line */
 preact.render(<App />, document.getElementById('root') as HTMLElement);
