8297d1c5addf19b231cb8d40628d1e57a3c5cd73
ebkn <ktennis.mqekr12@gmail.com>
2019-11-01 02:07:25 +0900

    feat: add js, precache

--- build/service-worker.js
+++ build/service-worker.js
@@ -1,7 +1,12 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
 if (workbox) {
+  workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
   workbox.googleAnalytics.initialize();
+  workbox.routing.registerRoute(
+    /.js/,
+    new workbox.strategies.NetworkFirst(),
+  );
   workbox.routing.registerRoute(
     /\.(?:png|gif|jpg|jpeg|svg|ico)$/,
     new workbox.strategies.CacheFirst({
