6d2d69ca241966cfa4a27b0242588a21651e53fb
ebkn <ktennis.mqekr12@gmail.com>
2019-11-01 01:37:46 +0900

    feat: add cache plugin for images

--- build/service-worker.js
+++ build/service-worker.js
@@ -1,6 +1,19 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+
 if (workbox) {
   workbox.googleAnalytics.initialize();
+  workbox.routing.registerRoute(
+    /\.(?:png|gif|jpg|jpeg|svg)$/,
+    new workbox.strategies.CacheFirst({
+      cacheName: 'images',
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 60,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+        }),
+      ],
+    }),
+  );
 } else {
   console.log('failed to load workbox');
 }
