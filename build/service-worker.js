7cbad5b1f450d4fda75719a90bd339d372fa0c96
ebkn <ktennis.mqekr12@gmail.com>
2019-11-01 01:40:39 +0900

    feat: add cache font

--- build/service-worker.js
+++ build/service-worker.js
@@ -3,15 +3,21 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox
 if (workbox) {
   workbox.googleAnalytics.initialize();
   workbox.routing.registerRoute(
-    /\.(?:png|gif|jpg|jpeg|svg)$/,
+    /\.(?:png|gif|jpg|jpeg|svg|ico)$/,
     new workbox.strategies.CacheFirst({
       cacheName: 'images',
       plugins: [
         new workbox.expiration.Plugin({
           maxEntries: 60,
-          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+          maxAgeSeconds: 30 * 24 * 60 * 60,
         }),
       ],
+    }),
+  );
+  workbox.routing.registerRoute(
+    'https://fonts.googleapis.com/css?family=Source+Code+Pro',
+    new workbox.strategies.CacheFirst({
+      cacheName: 'font',
     }),
   );
 } else {
