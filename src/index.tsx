943be6679f21e61c98e91bf9371b714be63bf01e
ebkn <ktennis.mqekr12@gmail.com>
2019-11-01 01:08:24 +0900

    fix: index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
-import { h, FunctionComponent } from 'preact';
+import * as preact from 'preact';
 import 'normalize.css';
 import App from './app';
 
-/** @jsx h */
+/* @jsx preact.h */
 /* eslint-disable-next-line */
 preact.render(<App />, document.getElementById('root') as HTMLElement);
