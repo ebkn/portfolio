5964d0714e5f1ebf8b97eac5c3f2939f5276f4c0
ebkn <ktennis.mqekr12@gmail.com>
2019-11-01 01:25:08 +0900

    feat: eslint-disable

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
-import * as preact from 'preact';
-import { h } from 'preact';
+import * as preact from 'preact'; // eslint-disable-line no-duplicate-imports
+import { h } from 'preact'; // eslint-disable-line no-duplicate-imports
 import 'normalize.css';
 import App from './app';
 
-/* @jsx h */
+/** @jsx h */
 /* eslint-disable-next-line */
 preact.render(<App />, document.getElementById('root') as HTMLElement);
