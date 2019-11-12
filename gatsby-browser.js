ee41db691f44a45a5646afe6511b70f702462a3e
ebkn <ktennis.mqekr12@gmail.com>
2019-11-13 01:56:59 +0900

    feat: setup firebase at gatsby-browser

---
+++ gatsby-browser.js
@@ -0,0 +1,18 @@
+const firebase = require('firebase/app');
+require('firebase/analytics');
+require('firebase/performance');
+
+export const onClientEntry = () => {
+  const firebaseConfig = {
+    apiKey: 'AIzaSyC5vsB3ECXxMcLSTiaEhmYKkke0z8XEE0s',
+    authDomain: 'portfolio-ebiken.firebaseapp.com',
+    databaseURL: 'https://portfolio-ebiken.firebaseio.com',
+    projectId: 'portfolio-ebiken',
+    storageBucket: 'portfolio-ebiken.appspot.com',
+    messagingSenderId: '476117283723',
+    appId: '1:476117283723:web:bead1f98dc0b97585b758e',
+    measurementId: 'G-5NNVVXSKGF',
+  };
+  firebase.initializeApp(firebaseConfig);
+  firebase.performance();
+};
