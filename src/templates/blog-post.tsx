1424a9d2d0fd73d15a2a2f9b11340d7fc6446df0
ebkn <ktennis.mqekr12@gmail.com>
2019-11-13 01:18:38 +0900

    feat: create blog-post template component

---
+++ src/templates/blog-post.tsx
@@ -0,0 +1,60 @@
+import { h, FunctionComponent } from 'preact';
+import { graphql, Link } from 'gatsby';
+
+interface Props {
+  data: any;
+  pageContext: any;
+  location: any;
+}
+
+/** @jsx h */
+const BlogPostTemplate: FunctionComponent<Props> = ({ data, pageContext, location }) => {
+  const post = data.markdownRemark;
+  const siteTitle = data.site.siteMetadata.title;
+
+  return (
+    <div>
+      <header>
+        <h3>
+          <Link to="/">
+            {siteTitle}
+          </Link>
+        </h3>
+      </header>
+      <main>
+        <article>
+          <header>
+            <h1>
+              {post.frontmatter.title}
+            </h1>
+            <p>
+              {post.frontmatter.date}
+            </p>
+          </header>
+          <section dangerouslySetInnerHTML={{ __html: post.html }} />
+        </article>
+      </main>
+    </div>
+  );
+};
+export default BlogPostTemplate;
+
+export const pageQuery = graphql`
+  query BlogPostBySlug($slug: String!) {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      id
+      excerpt(pruneLength: 160)
+      html
+      frontmatter {
+        title
+        date(formatString: "MMMM DD, YYYY")
+        description
+      }
+    }
+  }
+`;
