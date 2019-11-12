195d9dd39db2289e047be22a4b4f5f80bc9f4c59
ebkn <ktennis.mqekr12@gmail.com>
2019-11-13 00:25:09 +0900

    feat: create gatsby-config.js

---
+++ gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: 'blog',
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: 'assets',
+      },
+    },
+  ],
+};
