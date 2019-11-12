75083d39eb2c3cfcbc725a6a44293b53a283c284
ebkn <ktennis.mqekr12@gmail.com>
2019-11-13 01:33:50 +0900

    feat: rename not found to 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,17 +1,21 @@
 import { h, FunctionComponent } from 'preact';
 import styled from 'styled-components';
-import Title from '../atoms/title';
+import Layout from '../layout';
+import Title from '../components/atoms/title';
 
 /** @jsx h */
 const NotFoundPage: FunctionComponent<{}> = () => (
-  <StyledWrapper>
-    <Title title="404 NOT FOUND" />
-  </StyledWrapper>
+  <Layout>
+    <StyledWrapper>
+      <Title title="404 NOT FOUND" />
+    </StyledWrapper>
+  </Layout>
 );
+export default NotFoundPage;
+
 const StyledWrapper: any = styled.div`
   padding: 100px 0;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
-export default NotFoundPage;
