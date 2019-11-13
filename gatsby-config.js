d7c6c47b00e04ed25ff8e9fe20cea1635606cf8e
ebkn <ktennis.mqekr12@gmail.com>
2019-11-14 00:44:24 +0900

    feat: install react-helmet

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,6 +33,7 @@ module.exports = {
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     'gatsby-plugin-typescript',
+    'gatsby-plugin-react-helmet',
     {
       resolve: 'gatsby-plugin-graphql-codegen',
       options: {
