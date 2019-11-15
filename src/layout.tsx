7db2f1e51f33f58b8c887e4de4b6b92401618142
ebkn <ktennis.mqekr12@gmail.com>
2019-11-15 20:11:01 +0900

    feat: remove sidebar

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react';
 import styled from 'styled-components';
-import { isMobile } from './utils';
-import MobileSideBar from './components/molecules/mobileSideBar';
-import PcSideBar from './components/molecules/pcSideBar';
+import Header from './components/molecules/header';
 import Copyright from './components/molecules/copyright';
 import 'normalize.css';
 import './layout.css';
@@ -12,21 +10,10 @@ interface Props {
 }
 
 const Layout: React.FC<Props> = ({ children }) => {
-  const [sideBarOpen, updateSideBarOpen] = React.useState<boolean>(false);
-
-  const toggleSideBarOpen = (e: MouseEvent): void => {
-    e.preventDefault();
-    updateSideBarOpen(!sideBarOpen);
-  };
-
   return (
     <StyledContainer>
-      {isMobile() ? (
-        <MobileSideBar open={sideBarOpen} toggleOpen={toggleSideBarOpen} />
-      ) : (
-        <PcSideBar />
-      )}
-      <StyledMain sideBarOpen={sideBarOpen}>
+      <Header title="ebiken" />
+      <StyledMain>
         {children}
         <Copyright />
       </StyledMain>
@@ -36,14 +23,10 @@ const Layout: React.FC<Props> = ({ children }) => {
 export default Layout;
 
 const StyledContainer: any = styled.div`
-  display: flex;
   width: 100vw;
 `;
-type MainType = { sideBarOpen: boolean };
-const StyledMain: any = styled.main.attrs<MainType>({
-  style: ({ sideBarOpen }: MainType) => (isMobile() && !sideBarOpen ? { marginLeft: '0' } : { marginLeft: '30%' }),
-})`
+const StyledMain: any = styled.main`
   width: 100%;
   box-sizing: border-box;
-  padding: 30px 10px 20px 10px;
+  padding: 10px;
 `;
--- src/utils.ts
+++
@@ -1,2 +0,0 @@
-/* eslint-disable-next-line import/prefer-default-export */
-export const isMobile = (): boolean => (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(window.navigator.userAgent));
