8ca89f45df5eeae5f7c138d25934a4a897987d01
ebkn <ktennis.mqekr12@gmail.com>
2019-11-16 01:11:52 +0900

    feat: create html.tsx

---
+++ src/html.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+interface Props {
+  htmlAttributes: object;
+  headComponents: any;
+  bodyAttributes: object;
+  preBodyComponents: any[];
+  body: string;
+  postBodyComponents: any[];
+}
+
+const HTML = (props: Props) => (
+  <html {...props.htmlAttributes}>
+    <head>
+      <meta charSet="utf-8" />
+      <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+      {props.headComponents}
+    </head>
+    <body {...props.bodyAttributes} className="dark">
+      {props.preBodyComponents}
+      <noscript key="noscript" id="gatsby-noscript">
+        This app works best with JavaScript enabled.
+      </noscript>
+      <div key={`body`} id="___gatsby" dangerouslySetInnerHTML={{ __html: props.body }} />
+      {props.postBodyComponents}
+    </body>
+  </html>
+);
+export default HTML;
