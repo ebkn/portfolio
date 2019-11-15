9a0f045d9ab264c07a35683ea24b54f4ea2f0973
ebkn <ktennis.mqekr12@gmail.com>
2019-11-16 03:15:58 +0900

    feat: use png image for manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -92,44 +92,44 @@ module.exports = {
         icon: 'src/icon.jpg',
         icons: [
           {
-            'src': 'icons/icon-72x72.jpg',
+            'src': 'icons/icon-72x72.png',
             'sizes': '72x72',
-            'type': 'image/jpg'
+            'type': 'image/png'
           },
           {
-            'src': 'icons/icon-96x96.jpg',
+            'src': 'icons/icon-96x96.png',
             'sizes': '96x96',
-            'type': 'image/jpg'
+            'type': 'image/png'
           },
           {
-            'src': 'icons/icon-128x128.jpg',
+            'src': 'icons/icon-128x128.png',
             'sizes': '128x128',
-            'type': 'image/jpg'
+            'type': 'image/png'
           },
           {
-            'src': 'icons/icon-144x144.jpg',
+            'src': 'icons/icon-144x144.png',
             'sizes': '144x144',
-            'type': 'image/jpg'
+            'type': 'image/png'
           },
           {
-            'src': 'icons/icon-152x152.jpg',
+            'src': 'icons/icon-152x152.png',
             'sizes': '152x152',
-            'type': 'image/jpg'
+            'type': 'image/png'
           },
           {
-            'src': 'icons/icon-192x192.jpg',
+            'src': 'icons/icon-192x192.png',
             'sizes': '192x192',
-            'type': 'image/jpg'
+            'type': 'image/png'
           },
           {
-            'src': 'icons/icon-384x384.jpg',
+            'src': 'icons/icon-384x384.png',
             'sizes': '384x384',
-            'type': 'image/jpg'
+            'type': 'image/png'
           },
           {
-            'src': 'icons/icon-512x512.jpg',
+            'src': 'icons/icon-512x512.png',
             'sizes': '512x512',
-            'type': 'image/jpg'
+            'type': 'image/png'
           },
         ],
       },
